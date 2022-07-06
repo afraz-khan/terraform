7aa03c9fd1eff3694a19dbf679af5227b6d0e656
Afraz Khan <ah.afrazkhan@gmail.com>
2022-07-06 18:05:30 +0500

    adding instance ids

--- http-alb/output.tf
+++ http-alb/output.tf
@@ -1,3 +1,11 @@
 output "alb-dns-name" {
 	value = aws_lb.application-lb.dns_name
 }
+
+output "instance_id1" {
+	value = aws_instance.web-server[0].id
+}
+
+output "instance_id2" {
+	value = aws_instance.web-server[1].id
+}
